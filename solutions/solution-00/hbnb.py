77a8a2a30b04f2b97cbbae7ba8a3dad9d9ae6bef
root <root@FAT>
2024-06-25 11:10:26 +0200

    hbnb.py

--- solutions/solution-00/hbnb.py
+++ solutions/solution-00/hbnb.py
@@ -1,8 +1,15 @@
 """ Another way to run the app"""
 
 from src import create_app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = create_app()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///development.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 if __name__ == "__main__":
     app.run()
